d071785b99520158746f06bfb3b128f007e6ee28
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-03-01 15:58:51 +0900

    feat: Reja loyihamizning yakuniy bosqich logic

--- app.js
+++ app.js
@@ -1,72 +1,81 @@
 console.log("Let's start the project");
+
 const express = require('express');
 const app = express();
-
-// MongoDB connected
-const db = require("./server").db();
 const mongodb = require("mongodb");
+const db = require("./server").db(); // Ensure server.js exports `db()`
 
+// Middleware setup
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
+// Set EJS views
 app.set("views", "views");
 app.set("view engine", "ejs");
 
+// Create item
 app.post("/create-item", (req, res) => {
-    console.log("user entered /create-item")
+    console.log("User entered /create-item");
     console.log(req.body);
-    const new_reja = req.body.reja;
-    db.collection("plans").insertOne({ reja : new_reja}, (err, data) => {
-        res.json(data.ops[0]);
-    })
-});
 
-app.post("/delete-all", (req, res) => {
-    if(req.body.delete_all) {
-        db.collection("plans").deleteMany(function() {
-            res.json( {state: "all plans are deleted"});
-        });
+    const new_reja = req.body.reja.trim();
+    if (!new_reja) {
+        return res.json({ error: "Cannot add empty item" });
     }
+
+    db.collection("plans").insertOne({ reja: new_reja })
+        .then(data => res.json({ _id: data.insertedId, reja: new_reja }))
+        .catch(err => res.status(500).json({ error: "Database error" }));
 });
 
+// Delete item
 app.post("/delete-item", (req, res) => {
     const id = req.body.id;
-    db.collection("plans")
-    .deleteOne({_id: new mongodb.ObjectId(id)}, 
-    function (err, data) {
-            res.json({ state: "success" });
-        });   
+    
+    db.collection("plans").deleteOne({ _id: new mongodb.ObjectId(id) })
+        .then(() => res.json({ state: "success" }))
+        .catch(err => res.status(500).json({ error: "Delete failed" }));
 });
 
+// Edit item
 app.post("/edit-item", (req, res) => {
-  const data = req.body;
-  console.log(data);
-  db.collection("plans").findOneAndUpdate(
-    { _id: new mongodb.ObjectId(data.id) },
-    { $set: { reja: data.new_input } },
-    function (err, data) {
-      res.json({ state: "success" });
+    const { id, new_input } = req.body;
+
+    db.collection("plans").findOneAndUpdate(
+        { _id: new mongodb.ObjectId(id) },
+        { $set: { reja: new_input.trim() } }
+    )
+    .then(() => res.json({ state: "success" }))
+    .catch(err => res.status(500).json({ error: "Edit failed" }));
+});
+
+// Delete all
+app.post("/delete-all", (req, res) => {
+    if (req.body.delete_all) {
+        db.collection("plans").deleteMany({})
+            .then(() => res.json({ state: "All plans are deleted" }))
+            .catch(err => res.status(500).json({ error: "Delete-all failed" }));
     }
-  );
 });
 
+// Author page (fix undefined `user`)
 app.get("/author", (req, res) => {
-    res.render("author", { user: user });
+    const user = { name: "Your Name", email: "your@email.com" }; // Replace with actual user info
+    res.render("author", { user });
 });
 
-app.get("/", function (req, res) {
-    console.log("user entered /")
-    db.collection("plans")
-    .find()
-    .toArray((err, data) => {
-        if(err) {
-            console.log("Something went wrong");
-            res.end(err);
-        }else {
-            res.render("reja", { items: data});
-        }
-    });
+// Render main page
+app.get("/", (req, res) => {
+    console.log("User entered /");
+
+    db.collection("plans").find().toArray()
+        .then(data => res.render("reja", { items: data }))
+        .catch(err => {
+            console.error("Something went wrong:", err);
+            res.status(500).send("Internal server error");
+        });
 });
 
+// Export Express app
 module.exports = app;
